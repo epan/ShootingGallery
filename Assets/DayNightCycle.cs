eec9381855f0626a9f107984d5693bf1dc339952
Eric Pan <ericpanbot@gmail.com>
2016-12-11 19:16:31 -0800

    First commit
    2nd project at VR dev weekend

---
+++ Assets/DayNightCycle.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DayNightCycle : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		this.transform.Rotate (new Vector3 (3f * Time.deltaTime, 0f, 0f));
+
+//        Vector3 eulerRot = this.transform.rotation.eulerAngles;
+//        eulerRot.x += 3f * Time.deltaTime;
+//        this.transform.rotation = Quaternion.Euler(eulerRot);
+	}
+}
